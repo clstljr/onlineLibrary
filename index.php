fd5016f9da58d8c532397bbc4f61c56f797e6cdf
KeMeNoToRiKo <ombrogxavieremmanuel@gmail.com>
2025-04-30 23:17:02 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Libro - Your Online Library</title>
+  <link rel="stylesheet" href="assets/css/index.css"> <!-- Link to external CSS -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+</head>
+<body>
+
+  <div class="headerContainer"> 
+    <header><h1>Libro</h1></header>
+    <img src="assets/images/logo.jpg" alt="Logo" class="logo">
+    <div class="navContainer"> 
+      <nav> 
+        <ul> 
+          <li><a href="index.php">Home</a></li> 
+          <li><a href="about.php">About</a></li> 
+          <li><a href="contact.php">Contact</a></li> 
+        </ul> 
+      </nav>
+    </div>
+  </div>
+
+  <main>
+    <p>This is the main content area.</p>
+    <div class="loginSignupContainer">
+      <a href="login.php" class="loginButton">Login</a>
+      <a href="signup.php" class="signupButton">Sign Up</a>
+    </div>
+  </main>
+
+  <footer>
+    <p> &copy; 2025 Libro. All rights reserved. Designed by Group 0.</p>
+  </footer>
+
+</body>
+</html>
